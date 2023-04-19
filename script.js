8c86186ebb19b41a268b3f845c74397f28cad939
oapclothingco <131229506+oapclothingco@users.noreply.github.com>
2023-04-19 16:44:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 // Define an array of testimonials
 const testimonials = [
-  {
-    name: "John Doe",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit urna eget magna blandit, id venenatis urna blandit."
-  },
-  {
-    name: "Jane Smith",
-    text: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In euismod commodo metus, nec sollicitudin turpis iaculis eget."
-  },
-  {
-    name: "Bob Johnson",
-    text: "Nulla facilisi. Suspendisse maximus imperdiet eros, ac gravida justo vestibulum id. Aenean eleifend felis a nibh rhoncus pharetra."
-  }
+{
+name: "John Doe",
+text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit urna eget magna blandit, id venenatis urna blandit."
+},
+{
+name: "Jane Smith",
+text: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In euismod commodo metus, nec sollicitudin turpis iaculis eget."
+},
+{
+name: "Bob Johnson",
+text: "Nulla facilisi. Nullam sed blandit tortor, non interdum metus. Suspendisse consectetur est sit amet ex vestibulum, a venenatis dolor facilisis."
+}
 ];
 
-// Get the button element
-const button = document.querySelector("#show-testimonials");
+// Get the "Leave a Review" button element
+const leaveReviewBtn = document.getElementById("leave-review-btn");
 
-// Add a click event listener to the button
-button.addEventListener("click", () => {
-  // Create the pop-up window
-  const popup = window.open("", "Testimonials", "width=600,height=400");
+// Add an event listener to the button
+leaveReviewBtn.addEventListener("click", function() {
+// Get the div that will contain the form
+const formContainer = document.getElementById("review-form-container");
 
-  // Create the HTML content for the pop-up window
-  const content = `
-    <h2>Testimonials</h2>
-    <ul>
-      ${testimonials.map(testimonial => `<li><strong>${testimonial.name}:</strong> ${testimonial.text}</li>`).join('')}
-    </ul>
-  `;
+// Create the form elements
+const form = document.createElement("form");
+const nameInput = document.createElement("input");
+const reviewInput = document.createElement("textarea");
+const submitBtn = document.createElement("button");
 
-  // Add the content to the pop-up window
-  popup.document.body.innerHTML = content;
-});
-document.getElementById("review-btn").addEventListener("click", function() {
-  this.style.display = "none";
+// Set the attributes for the form elements
+form.setAttribute("id", "review-form");
+nameInput.setAttribute("type", "text");
+nameInput.setAttribute("id", "name-input");
+nameInput.setAttribute("placeholder", "Enter your name");
+reviewInput.setAttribute("id", "review-input");
+reviewInput.setAttribute("placeholder", "Enter your review");
+submitBtn.setAttribute("type", "submit");
+submitBtn.setAttribute("id", "submit-review-btn");
+submitBtn.textContent = "Submit";
+
+// Append the form elements to the form and the form to the container
+form.appendChild(nameInput);
+form.appendChild(reviewInput);
+form.appendChild(submitBtn);
+formContainer.appendChild(form);
+
+// Remove the button from the container
+formContainer.removeChild(leaveReviewBtn);
 });
